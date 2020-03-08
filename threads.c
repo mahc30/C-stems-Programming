612e187c6540c0a4df3be681f11f44a92014d95a
mahc30 <48729946+mahc30@users.noreply.github.com>
2020-03-07 22:32:01 -0500

    Update threads.c

--- threads.c
+++ threads.c
@@ -1,13 +1,27 @@
+ /*
+ ============================================================================
+ Name        : yes.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
  #include <unistd.h>
  #include <stdio.h>
  #include <stdlib.h>
  #include <pthread.h>
 
+typedef struct{
+	int lineas;
+}linea;
+
 void* contarLineas(void* arg){
 	 FILE *file = fopen(arg,"r");
 
 	 char buffer[20];
-	 intptr_t cont = 1;
+	 intptr_t cont = 0;
 	 while(fgets(buffer,20,file) != NULL){
 		 cont++;
 	 }
@@ -18,21 +32,21 @@ void* contarLineas(void* arg){
  int main(int argc, char *argv[])
 {
 	pthread_t id_hilo[argc];
-	int lineas[argc];
+	linea lineas[argc];
 
 	 for(int i = 1; i < argc; i++){
 		pthread_create(id_hilo + i, NULL, &contarLineas, argv[i]);
 	 }
 
 	for(int i = 1; i < argc; i++){
-		pthread_join(id_hilo[i], (void*)&lineas[i-1]);
+		pthread_join(id_hilo[i], (void*)&lineas[i-1].lineas);
 	}
 
 	int total = 0;
 
 	for(int i = 0; i < argc-1; i++){
 		total += lineas[i];
-		printf("\nArchivo %d: %d Lineas",i,lineas[i]);
+		printf("\nArchivo %d: %d Lineas",i,lineas[i].lineas);
 	}
 
 	printf("\nTotal Lineas: %d",total);
